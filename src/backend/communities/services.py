b5f6ce421262aadc71fca1be034a782ac719862b
NechepurenkoA <nechepurenko.work@gmail.com>
2024-02-05 18:23:41 +0300

    following unfollowing services

---
+++ src/backend/communities/services.py
@@ -0,0 +1,15 @@
+from .models import Community
+
+
+class FollowService(object):
+
+    def __init__(self, request):
+        self.request = request
+
+    def add_follower(self, community: Community) -> None:
+        community.followers.add(self.request.user)
+        return None
+
+    def remove_follower(self, community: Community) -> None:
+        community.followers.remove(self.request.user)
+        return None
