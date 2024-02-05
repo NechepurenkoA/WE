b895dadefa6c75f304bdd3771652857e91a49247
NechepurenkoA <nechepurenko.work@gmail.com>
2024-02-05 12:17:18 +0300

    api urls and token authentication urls

--- src/backend/users/urls.py
+++ src/backend/users/urls.py
@@ -1,6 +1,13 @@
 from django.conf import settings
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from users.api_views import UserSingUpViewSet, UserViewSet
+
+router = DefaultRouter()
+router.register(r"users", UserViewSet, basename="users")
+router.register(r"signup", UserSingUpViewSet, basename="signup")
 
 urlpatterns = [
     path(f"{settings.API_V1_PREFIX}/auth/", include("djoser.urls.authtoken")),
+    path(f"{settings.API_V1_PREFIX}/", include(router.urls)),
 ]
