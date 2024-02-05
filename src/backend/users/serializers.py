b10fc7c609f577701625598136fd857d8220609e
NechepurenkoA <nechepurenko.work@gmail.com>
2024-02-05 12:16:46 +0300

    serializers for user model

--- src/backend/users/serializers.py
+++ src/backend/users/serializers.py
@@ -1,22 +1,60 @@
+from django.contrib.auth import password_validation
 from rest_framework import serializers
 
-from .models import User
+from .models import Friendship, User
 
 
 class UserRetrieveSerializer(serializers.ModelSerializer):
     """
-    Сериализатор просмотра пользователя
+    Сериализатор модели 'User'.
     """
+
+    are_friends = serializers.SerializerMethodField(
+        read_only=True,
+        source="get_are_friends",
+    )
 
     class Meta:
         model = User
+        fields = (
+            "avatar",
+            "username",
+            "first_name",
+            "last_name",
+            "sex",
+            "age",
+            "biography",
+            "are_friends",
+        )
+
+    def get_are_friends(self, obj):
+        request = self.context["request"]
+        return Friendship.objects.filter(
+            who=request.user.id,
+            with_who=obj,
+        ).exists()
 
 
-class UserSignupSerializer(serializers.ModelSerializer):
-    """
-    Сериализатор регистрации пользователя
-    """
+class UserSignUpSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ("email", "username", "first_name", "last_name", "sex", "birth_date")
+        fields = (
+            "username",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "sex",
+            "age",
+            "birth_date",
+            "biography",
+        )
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "age": {"read_only": True},
+        }
+
+    def validate_password(self, data):
+        password_validation.validate_password(data)
+        return data
