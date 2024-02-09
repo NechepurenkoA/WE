225c6b6ad00af1998fe65a00ace4f00f48379930
NechepurenkoA <nechepurenko.work@gmail.com>
2024-02-09 15:56:43 +0300

    added posts urls

--- src/backend/WEB_social_network/urls.py
+++ src/backend/WEB_social_network/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("communities.urls")),
+    path("api/", include("posts.urls")),
 ]
