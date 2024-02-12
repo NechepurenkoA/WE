9bd8004a56e7252b4a92abd0ab563fa85f90862f
NechepurenkoA <nechepurenko.work@gmail.com>
2024-02-12 18:44:02 +0300

    minor permission change

--- src/backend/users/permissions.py
+++ src/backend/users/permissions.py
@@ -1,14 +1,12 @@
 from rest_framework import permissions
 
 
-class IsAuthenticatedOrAdmin(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS or request.user.is_superuser
+class IsAuthenticatedOrAdminForUsers(permissions.IsAuthenticated):
+    """Доступ к просмотру пользователей."""
 
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
             or request.user.is_superuser
-            or obj.id == request.user.id
+            or obj == request.user
         )
